4ba962de3cf8970a61d3e023dc7a65c17355233a
Muskanagrawal263 <muskanagrawal063@gmail.com>
2025-07-25 16:22:42 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2> Order Your Fav Food Here </h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with finest ingredients. Our mission is to satisfy your cravings </p>
+  
+        <button> View menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
